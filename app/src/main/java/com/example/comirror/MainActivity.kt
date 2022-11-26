58912d0bca07b1c689d6c5e4f47edf3907654ddf
kyum-q <109158497+kyum-q@users.noreply.github.com>
2022-11-27 00:58:48 +0900

    워치 알림 설정 and home

--- app/src/main/java/com/example/comirror/MainActivity.kt
+++ app/src/main/java/com/example/comirror/MainActivity.kt
@@ -3,9 +3,12 @@ package com.example.comirror
 import android.app.Activity
 import android.os.Bundle
 import com.example.comirror.databinding.ActivityMainBinding
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : Activity() {
-
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +16,25 @@ class MainActivity : Activity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        getFCMToken()
 
+    }
+    private fun getFCMToken(): String?{
+        var token: String? = null
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+
+            // Get new FCM registration token
+            token = task.result
+
+            // Log and toast
+            println( "FCM Token is ${token}")
+
+            val map = mutableMapOf<String, Any>()
+            map["pushToken"] = token!!
+
+            FirebaseFirestore.getInstance().collection("pushTokens").document().set(map)
+        })
+
+        return token
     }
 }
