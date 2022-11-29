76b22d12fd45a853e2f539da57db3f342608f0f0
syy0O <songie05@naver.com>
2022-11-30 02:35:59 +0900

    mqtt test

--- app/src/main/java/com/example/comirror/data/MessageDTO.kt
+++ app/src/main/java/com/example/comirror/data/MessageDTO.kt
@@ -1,7 +1,6 @@
 package com.example.comirror.data
 
 data class MessageDTO(
-        var sendId: String="",
         var senderName: String="",
         var content: String="",
 )
--- app/src/main/java/com/example/comirror/home/MainActivity.kt
+++ app/src/main/java/com/example/comirror/home/MainActivity.kt
@@ -3,23 +3,32 @@ package com.example.comirror.home
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import com.example.comirror.databinding.ActivityMainBinding
 import com.example.comirror.message.MessageActivity
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.messaging.FirebaseMessaging
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken
+import org.eclipse.paho.client.mqttv3.MqttCallback
+import org.eclipse.paho.client.mqttv3.MqttClient
+import org.eclipse.paho.client.mqttv3.MqttMessage
 import org.json.JSONObject
 
 class MainActivity : Activity() {
     private lateinit var binding: ActivityMainBinding
+
+
+    val ServerIP:String = "tcp://172.30.1.41:1883"  //1번 서버 IP
+    val TOPIC:String = "watch/4004"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        getFCMToken()
-        jsonToByteArray()
+        //getFCMToken()
+       // jsonToByteArray()
 
         binding.settingBtn.setOnClickListener {
             startActivity(Intent(this, SettingActivity::class.java))
@@ -27,6 +36,32 @@ class MainActivity : Activity() {
         binding.messageBtn.setOnClickListener {
             startActivity(Intent(this, MessageActivity::class.java))
         }
+
+        var mqttClient: MqttClient? = null
+        mqttClient = MqttClient(ServerIP, MqttClient.generateClientId(), null) //3번 연결설정
+        mqttClient.connect()
+
+        mqttClient.subscribe(TOPIC) //5번 구독 설정
+        mqttClient.setCallback(object : MqttCallback { //6번 콜백 설정
+            override fun connectionLost(p0: Throwable?) {
+                //연결이 끊겼을 경우
+                Log.d("MQTTService","Connection Lost")
+            }
+
+            override fun messageArrived(p0: String?, p1: MqttMessage?) {
+                //메세지가 도착했을 때 여기
+                Log.d("MQTTService","여기 들어옴")
+//                Log.d("MQTTService","Message Arrived : " + p1.toString()) //7번 메세지 도착
+            }
+
+            override fun deliveryComplete(p0: IMqttDeliveryToken?) {
+                //메세지가 도착 하였을 때
+                Log.d("MQTTService","Delivery Complete")
+            }
+
+        })
+
+
     }
     private fun jsonToByteArray() {
         var json: JSONObject = JSONObject()
