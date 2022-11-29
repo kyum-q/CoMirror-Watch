6f8c87c825eb2a89176e3d9afd5887311b93f10e
kyum-q <109158497+kyum-q@users.noreply.github.com>
2022-11-29 23:04:44 +0900

    login activity

---
+++ app/src/main/java/com/example/comirror/LoginActivity.kt
@@ -0,0 +1,22 @@
+package com.example.comirror
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import com.example.comirror.databinding.ActivityLoginBinding
+
+class LoginActivity : Activity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginBtutton.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+}
