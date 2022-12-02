1218588999200ebb809e78c352c44aa8f0a6aad3
kyum-q <109158497+kyum-q@users.noreply.github.com>
2022-12-03 01:20:08 +0900

    심박수 도전~~

--- app/src/main/java/com/example/comirror/home/MainActivity.kt
+++ app/src/main/java/com/example/comirror/home/MainActivity.kt
@@ -1,26 +1,38 @@
 package com.example.comirror.home
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
+import android.os.PowerManager
 import android.util.Log
-import com.example.comirror.MirrorApplication
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
 import com.example.comirror.data.MessageDTO
 import com.example.comirror.databinding.ActivityMainBinding
 import com.example.comirror.message.MessageActivity
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.fitness.request.OnDataPointListener
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.common.reflect.TypeToken
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.gson.GsonBuilder
-import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken
-import org.eclipse.paho.client.mqttv3.MqttCallback
-import org.eclipse.paho.client.mqttv3.MqttClient
-import org.eclipse.paho.client.mqttv3.MqttMessage
-import org.json.JSONObject
 import java.lang.reflect.Type
 
-class MainActivity : Activity() {
+
+class MainActivity : Activity(), SensorEventListener {
+    private var mTextViewHeart: TextView? = null
+    var mSensorManager: SensorManager? = null
+    var mHeartRateSensor: Sensor? = null
+    var sensorEventListener: SensorEventListener? = null
+
     private lateinit var binding: ActivityMainBinding
 
     val ServerIP:String = "tcp://192.168.0.2:1883" //서버 IP
@@ -31,11 +43,60 @@ class MainActivity : Activity() {
     private var pref: String? = null
     private var messageDataList: ArrayList<MessageDTO> = arrayListOf()
 
+    //
+    private var TAG: String? = MainActivity::class.java.name
+    private var googleApiClient: GoogleApiClient? = null
+    private var authInProgress = false
+    private var onDataPointListener: OnDataPointListener? = null
+    private val AUTH_REQUEST = 1
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        Log.d(TAG, "onAccuracyChanged - accuracy: $accuracy")
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        if (event.sensor.type == Sensor.TYPE_HEART_RATE) {
+            val msg = "" + event.values[0].toInt()
+            mTextViewHeart!!.text = msg
+            Log.d(TAG, msg)
+        } else Log.d(TAG, "Unknown sensor type")
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
+    private val REQUEST_PERMISSION_CODE = 12345
+    private val missingPermission: ArrayList<String> = ArrayList()
+
+    private var bCheckStarted = false
+    private var bGoogleConnected = false
+
+    private var btnStart: Button? = null
+    private var spinner: ProgressBar? = null
+    private var powerManager: PowerManager? = null
+    private var wakeLock: PowerManager.WakeLock? = null
+    private var textMon: TextView? = null
+
+
+    @SuppressLint("InvalidWakeLockTag")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        mTextViewHeart = binding.textView2
+        mSensorManager = getSystemService(SENSOR_SERVICE) as SensorManager?
+        mHeartRateSensor = mSensorManager!!.getDefaultSensor(Sensor.TYPE_HEART_RATE)
+        mSensorManager!!.registerListener(this, mHeartRateSensor, SensorManager.SENSOR_DELAY_NORMAL)
+        Log.i(TAG, "LISTENER REGISTERED.")
+        mTextViewHeart!!.text = "Something here"
+        mSensorManager!!.registerListener(
+            sensorEventListener,
+            mHeartRateSensor,
+            SensorManager.SENSOR_DELAY_FASTEST
+        )
 
         //getFCMToken()
 
@@ -46,6 +107,7 @@ class MainActivity : Activity() {
             startActivity(Intent(this, MessageActivity::class.java))
         }
 
+        /*
         var mqttClient: MqttClient? = null
         mqttClient = MqttClient(ServerIP, MqttClient.generateClientId(), null) //3번 연결설정
         mqttClient.connect()
@@ -87,8 +149,17 @@ class MainActivity : Activity() {
                 Log.d("MQTTService","Delivery Complete")
             }
 
-        })
 
+        })
+        */
+
+
+        //심박패턴을 측정하는 동안 화면이 꺼지지 않도록 제어하기 위해 전원관리자를 얻어옵니다
+        powerManager = getSystemService(POWER_SERVICE) as PowerManager
+        wakeLock = powerManager!!.newWakeLock(
+            PowerManager.SCREEN_BRIGHT_WAKE_LOCK or PowerManager.ACQUIRE_CAUSES_WAKEUP or PowerManager.ON_AFTER_RELEASE,
+            "WAKELOCK"
+        )
     }
 
     private fun getFCMToken(): String?{
@@ -106,5 +177,22 @@ class MainActivity : Activity() {
             FirebaseFirestore.getInstance().collection("pushTokens").document("Token").set(map)
         })
         return token
+    }
+
+
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onStart connect attempted")
+    }
+
+
+    @Synchronized
+    private fun addContentToView(value: Float) {
+        runOnUiThread {
+            if (spinner!!.visibility == View.VISIBLE) spinner!!.visibility = View.INVISIBLE
+            Log.d(TAG, "Heart Beat Rate Value : $value")
+            textMon!!.text = "Heart Beat Rate Value : $value"
+        }
     }
 }
